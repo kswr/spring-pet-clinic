331ec54d1e8a7194770d29ab4dd0aa7cf5562190
kswr <ksawery.oleszynski@gmail.com>
2019-02-13 15:36:05 +0100

    refactored services to common base Crud Service

--- pet-clinic-data/src/main/java/com/kswr/pet/clinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/kswr/pet/clinic/services/OwnerService.java
@@ -2,12 +2,7 @@ package com.kswr.pet.clinic.services;
 
 import com.kswr.pet.clinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
