30ad93cfe8ebe971580f1a9f990da23d2958b357
kswr <ksawery.oleszynski@gmail.com>
2019-02-20 00:19:23 +0100

    added not implemented handler page

--- pet-clinic-web/src/main/java/com/kswr/pet/clinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/kswr/pet/clinic/controllers/OwnerController.java
@@ -15,10 +15,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
+
 
 }
