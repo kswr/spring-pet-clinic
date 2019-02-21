6ac5a339b3c47dcc5898452502dd5093efb41038
kswr <ksawery.oleszynski@gmail.com>
2019-02-21 14:18:49 +0100

    added basic orm with hibernate

--- pet-clinic-data/src/main/java/com/kswr/pet/clinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/kswr/pet/clinic/model/BaseEntity.java
@@ -1,9 +1,13 @@
 package com.kswr.pet.clinic.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
